ad11894eb6ebe67a09d1c9cb3f08ff3570a643b5
azaamabubacker <cleancodelk@gmail.com>
2023-06-03 13:27:42 +0530

    Fix the bug which is the date

--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -6,7 +6,7 @@ import ExpensesChart from "./ExpensesChart";
 import ExpensesList from "./ExpensesList";
 
 function Expenses(props) {
-  const [filteredYear, setFilteredYear] = useState("2020");
+  const [filteredYear, setFilteredYear] = useState("2023");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
